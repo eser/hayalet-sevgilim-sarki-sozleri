7b6befa3cb9bb173e4e08c39e6e38a349b0258f4
Ömer Hamid Kamışlı <hamid.kamisli@magis.com.tr>
2022-06-22 22:18:55 +0300

    Initial Commit

---
+++ hayaletsevgilim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hayaletsevgilim
+//
+//  Created by Ömer Hamid Kamışlı on 22.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
