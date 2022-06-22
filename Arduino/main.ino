35baaba2faec4f497938ec66590a3f9df77b43c9
celikozkan <ozkan44celik@gmail.com>
2022-06-22 18:20:43 +0300

    Add: Arduino version

---
+++ Arduino/main.ino
@@ -0,0 +1,64 @@
+void setup(){
+    Serial.begin(9600);
+    Serial.println('HAYALET SEVGILIM');
+    Serial.println();
+    Serial.println("Ceza mı bu");
+    Serial.println("Çektiğim çile mi");
+    Serial.println("Yıllardır tuttuğum nöbet bitmeyecek mi?");
+    Serial.println("Bir küçük kar tanesi gibiyim");
+    Serial.println("Avucunda eriyen dön bebeğim");
+    Serial.println();
+    Serial.println("Gözyaşlarını görürsem");
+    Serial.println("Erir kanatlarım");
+    Serial.println("Uçamam rüyalarında yanına");
+    Serial.println("Sonsuzluk senle başladı");
+    Serial.println("O küçük dünyamda");
+    Serial.println("Unutma gittiğinde yarım kaldım");
+    Serial.println();
+    Serial.println("Çöllerdeyim yanıyorum");
+    Serial.println("Kutuptayım üşüyorum");
+    Serial.println("Ceza benim çekiyorum ne olur dön");
+    Serial.println("Uzanıyorum tutamıyorum");
+    Serial.println("Özlüyorum ağlıyorum");
+    Serial.println("Yasak mısın anlamıyorum ne olur dön");
+    Serial.println();
+    Serial.println("Sevmesen de beni özledim sesini");
+    Serial.println("Git desem de yine gitmesen");
+    Serial.println("Yıllardır çektiğim bu hasret mi çile mi?");
+    Serial.println("Haram mısın bana bi' bilsem");
+    Serial.println();
+    Serial.println("Sevmesen de beni özledim sesini");
+    Serial.println("Git desem de yine gitmesen");
+    Serial.println("Yıllardır çektiğim bu hasret mi çile mi?");
+    Serial.println("Haram mısın bana bi' bilsem");
+    Serial.println();
+    Serial.println("Bebeğim benim, hayalet sevgilim");
+    Serial.println("Bebeğim benim, hayalet sevgilim");
+    Serial.println();
+    Serial.println("Hayalet sevgilim");
+    Serial.println();
+    Serial.println("Çöllerdeyim yanıyorum");
+    Serial.println("Kutuptayım üşüyorum");
+    Serial.println("Ceza benim çekiyorum ne olur dön");
+    Serial.println("Uzanıyorum tutamıyorum");
+    Serial.println("Özlüyorum ağlıyorum");
+    Serial.println("Yasak mısın anlamıyorum ne olur dön");
+    Serial.println();
+    Serial.println("Sevmesen de beni özledim sesini");
+    Serial.println("Git desem de yine gitmesen");
+    Serial.println("Yıllardır çektiğim bu hasret mi çile mi?");
+    Serial.println("Haram mısın bana bi' bilsem");
+    Serial.println();
+    Serial.println("Sevmesen de beni özledim sesini");
+    Serial.println("Git desem de yine gitmesen");
+    Serial.println("Yıllardır çektiğim bu hasret mi çile mi?");
+    Serial.println("Haram mısın bana bi' bilsem");
+    Serial.println();
+    Serial.println("Bebeğim benim, hayalet sevgilim");
+    Serial.println("Bebeğim benim, hayalet sevgilim");
+    Serial.println();
+    Serial.println("Hayalet sevgilim");
+    Serial.println("Hayalet sevgilim")
+}
+void loop(){
+}
